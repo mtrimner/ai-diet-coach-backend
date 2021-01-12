94e69454628b3514b576f00809ac71a0a64dee3f
mtrimner <matt.trimner@gmail.com>
2021-01-11 18:13:13 -0800

    added user information to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_09_185529) do
+ActiveRecord::Schema.define(version: 2021_01_12_021151) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "username"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "date_of_birth"
+    t.string "sex"
+    t.integer "height"
   end
 
 end

c94f066981a4b46f9d68889783eddb0598fc322b
mtrimner <matt.trimner@gmail.com>
2021-01-14 20:21:26 -0800

    added macro calculation file

--- app/controllers/macro_params_controller.rb
+++ app/controllers/macro_params_controller.rb
@@ -1,10 +1,17 @@
 class MacroParamsController < ApplicationController
-  skip_before_action :require_login
 
   def macros
-    user = User.find(1)
+    # user = User.find_by(id: 1)
 
-    tdee = TdeeCalculator.new(user).calculate_tdee
+    tdee = TdeeCalculator.new(@user).calculate_tdee
+    macros = MacroCalculations.new(@user, tdee).macro_calculation
     
+    render json: macros.to_json
   end
+end
+
+private
+
+def set_user
+  @user = User.find(params[:id])
 end
--- app/services/macro_calculations.rb
+++ app/services/macro_calculations.rb
@@ -5,6 +5,29 @@ class MacroCalculations
         @tdee = tdee
     end
 
-    
+    def macro_calculation
+        recommended = {
+        calories: @tdee,
+        macros: {
+            protein: protein,
+            carbs: carbs,
+            fat: fat
+        }
+    }
+    end
+
+    private
+
+    def protein
+        ((@tdee * 0.33) / 4).floor
+    end
+
+    def carbs
+        ((@tdee * 0.33) / 4).floor
+    end
+
+    def fat 
+        ((@tdee * 0.34) / 9).floor
+    end
 
 end
